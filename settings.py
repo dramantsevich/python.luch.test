5e75059b2b221fb54e3dc0c4751db5d026f28ca1
Dzmitry Ramantsevich <d.ramantsevich@gmail.com>
2022-01-14 21:58:40 +0300

    feat: add configuration for tests

---
+++ settings.py
@@ -0,0 +1,8 @@
+BASE_URL = 'https://luch.by/en'
+USER = {
+    'name': 'Dzmitrytest',
+    'surname': 'Dzmitrytest',
+    'phone': '123456789121',
+    'email': 'test@test.by',
+    'city': 'Minsk'
+}
