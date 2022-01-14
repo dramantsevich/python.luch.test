c6a078b353ba1b299b8b6d7654525ee2353880b4
Dzmitry Ramantsevich <d.ramantsevich@gmail.com>
2022-01-14 21:56:10 +0300

    feat: add fixtures for tests

---
+++ conftest.py
@@ -0,0 +1,36 @@
+from pytest import fixture
+from playwright.sync_api import sync_playwright
+from page_object.application import App
+import settings
+
+# @fixture(autouse=True, scope='session')
+# def preconditions():
+#     print('setup init state')
+#
+# @fixture(autouse=True, scope='session')
+# def postconditions():
+#     yield
+#     print('setup postcondition state')
+
+@fixture(scope='session')
+def get_playwright():
+    with sync_playwright() as playwright:
+        yield playwright
+
+@fixture(scope='session')
+def app(get_playwright, request):
+    base_url = request.config.getini('base_url')
+    app = App(get_playwright, base_url=base_url)
+    # app.goto('/')
+    yield app
+    app.close()
+
+# @fixture(scope='session')
+# def desktop_app_auth(desktop_app):
+#     app = desktop_app
+#     app.goto('/login')
+#     app.login('', '')
+#     yield app
+# **settings.USER #распаковка dictionary
+def pytest_addoption(parser):
+    parser.addini('base_url', help='base url of site under test', default='https://luch.by/en')
